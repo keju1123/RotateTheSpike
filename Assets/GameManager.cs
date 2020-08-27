7940b37284379eb6d3d72869b7fb456006ff4a1c
keju1123 <kelvinjulian.kj@gmail.com>
2020-08-27 22:38:28 +0700

    added animation & make smaller spike
    Tweaking size and speed

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -24,23 +24,29 @@ public class GameManager : MonoBehaviour
         scoreText.text = score.ToString();
         if(score % 5 == 0 && score!=savedScore)
         {
-            makeNewSpike();
+            if(spikeAmount <= 25)
+            {
+                makeNewSpike();
+            }
             savedScore = score;
         }
     }
 
     void makeNewSpike()
     {
-        
-        for(int i=0;i<spikes.Length;i++)
+        spikeAmount += addSpikePer5;
+        for (int i = 0; i < spikeAmount + addSpikePer5; i++)
         {
-            spikes[i].GetComponent<Animator>().Play("SpikeDissapear");
-        }
-        for(int i=0; i<spikeAmount+addSpikePer5;i++)
-        {
-            spikeAmount += addSpikePer5;
             int randnum = Random.Range(1, spikes.Length);
             spikes[randnum].GetComponent<Animator>().SetTrigger("Appear");
         }
+        for (int i = 0; i < spikes.Length; i++)
+        {
+            if(spikes[i].GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name.Equals("SpikeAppear"))
+            {
+                spikes[i].GetComponent<Animator>().Play("SpikeDissapear");
+            }
+        }
+
     }
 }
