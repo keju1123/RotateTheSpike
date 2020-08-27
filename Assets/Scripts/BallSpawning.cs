f44e9091675ecf27bc1da07360ced066a0bf1f70
keju1123 <kelvinjulian.kj@gmail.com>
2020-08-27 10:24:59 +0700

    ballSpawning updated

--- Assets/Scripts/BallSpawning.cs
+++ Assets/Scripts/BallSpawning.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BallSpawning : MonoBehaviour
 {
+    [SerializeField]
+    private float timeToStartSpawning = 1.5f;
+    [SerializeField]
+    private float timeBetweenSpawns = 1f;
+
+    private float tempTime;
+
+
+    public GameObject ball;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tempTime = timeToStartSpawning;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        tempTime -= Time.deltaTime;
+        if(tempTime <= 0)
+        {
+            tempTime = timeBetweenSpawns;
+        }
+    }
+
+    void SpawnBall()
+    {
+        timeBetweenSpawns += Random.Range(-0.4f, 0.2f);
+        Instantiate(ball, gameObject.transform.position, Quaternion.identity);
     }
 }
