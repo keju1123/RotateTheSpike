8ae4352aa770560d3f94234a666e998a685f38a5
keju1123 <kelvinjulian.kj@gmail.com>
2020-08-29 17:07:03 +0700

    Gem and playerpreffs
    shatter particle
    gem playerprefs

---
+++ Assets/Scripts/AddGem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddGem : MonoBehaviour
+{
+    int defGem = 0;
+    public GameObject particle;
+
+    [SerializeField]
+    private TextMeshProUGUI gemText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gemText = GameObject.FindGameObjectWithTag("GemText").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("sentuh");
+        if (collision.tag == "Catcher")
+        {
+            Instantiate(particle, transform.position, Quaternion.identity);
+            PlayerPrefs.SetInt("Gem", PlayerPrefs.GetInt("Gem", defGem) + 1);
+            Destroy(gameObject);
+
+            gemText.text = PlayerPrefs.GetInt("Gem", defGem).ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/ParticleSelfDestruct.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSelfDestruct : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem particle = GetComponent<ParticleSystem>();
+        ParticleSystem.MainModule thisPart = particle.main;
+        Destroy(gameObject, thisPart.duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
