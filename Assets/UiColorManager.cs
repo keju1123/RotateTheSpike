5f2ffa1e2775b09ff18dfac6c827c2fcff86d316
keju1123 <kelvinjulian.kj@gmail.com>
2020-08-30 10:24:40 +0700

    main menu

--- Assets/UiColorManager.cs
+++ Assets/UiColorManager.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using ColorData;
+using UnityEngine.UI;
 
 public class UiColorManager : MonoBehaviour
 {
     [SerializeField]
     private Camera maincam;
+    [SerializeField]
+    private Image circle;
+
+    [SerializeField]
+    private Image panel;
+
+    [SerializeField]
+    private Animator panelAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +23,11 @@ public class UiColorManager : MonoBehaviour
         string index = "Color" + randint.ToString();
         PlayerPrefs.SetString("Color", index);
         maincam.backgroundColor = ColorList.colorCollection[index].camAndSpikeColor;
+        circle.color = ColorList.colorCollection[index].circleColor;
+        panel.color = maincam.backgroundColor;
+
+
+        panelAnim.SetTrigger("OpenPanel");
     }
 
     // Update is called once per frame
